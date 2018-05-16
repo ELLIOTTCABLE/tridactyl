e287c182a697dbbe4838763922580f0927aee4c5
ELLIOTTCABLE <me@ell.io>
2018-05-16 13:17:20 -0500

    + begin implementing the handling of an extra, prepended colon on excmds

--- src/aliases.ts
+++ src/aliases.ts
@@ -3,8 +3,9 @@ import * as config from "./config"
 /**
  * Expands the alias in the provided exstr recursively. Does nothing if
  * the command is not aliased, including when the command is invalid.
+ * Returns another string.
  *
- * @param exstr :exstr typed by the user on the commantd line
+ * @param exstr :exstr typed by the user on the command line
  */
 export function expandExstr(
     exstr: string,
@@ -12,11 +13,33 @@ export function expandExstr(
     prevExpansions: string[] = [],
 ): string {
     // Split on whitespace
-    const [command, ...args] = exstr.trim().split(/\s+/)
+    let [command, ...args] = exstr.trim().split(/\s+/)
+
+    // Handle initial (extra) colon
+    if (command[0] === ":") {
+        command = command.substring(1)
+    }
+
+    return expandExarr([command, ...args]).join(" ")
+}
+
+/**
+ * Expands the alias in the provided exstr recursively. Does nothing if
+ * the command is not aliased, including when the command is invalid.
+ * Expects, and returns, an array of `command, ...args`.
+ *
+ * @param exarr :exstr typed by the user on the command line
+ */
+export function expandExarr(
+    exarr: string[],
+    aliases = config.get("exaliases"),
+    prevExpansions: string[] = [],
+): string[] {
+    const command = exarr[0]
 
     // Base case: alias not found; return original command
     if (aliases[command] === undefined) {
-        return exstr
+        return exarr
     }
 
     // Infinite loop detected
@@ -27,10 +50,8 @@ export function expandExstr(
     // Add command to expansions used so far
     prevExpansions.push(command)
 
+    exarr[0] = aliases[command]
+
     // Alias exists; expand it recursively
-    return expandExstr(
-        exstr.replace(command, aliases[command]),
-        aliases,
-        prevExpansions,
-    )
+    return expandExarr(exarr, aliases, prevExpansions)
 }
